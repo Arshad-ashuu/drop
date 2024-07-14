62bc0e6ebbf0a13fcb6fb02f6dc0367861e148b2
Arshad-ashuu <mohammadarshad01474@gmail.com>
2024-07-14 17:51:57 +0530

    connected backend

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,14 @@
-import { SplashScreen, Stack } from "expo-router"
-import { useFonts } from 'expo-font'
-import { useEffect } from 'react'
+import { useEffect } from "react";
+import { useFonts } from "expo-font";
+import "react-native-url-polyfill/auto";
+import { SplashScreen, Stack } from "expo-router";
 
+import GlobalProvider from "../context/GlobalProvider";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
-export default function RootLayout() {
 
+const RootLayout = () => {
     const [fontsLoaded, error] = useFonts({
         "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
         "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
@@ -19,16 +23,32 @@ export default function RootLayout() {
 
     useEffect(() => {
         if (error) throw error;
-        if (fontsLoaded) SplashScreen.hideAsync()
-    }, [fontsLoaded, error])
+
+        if (fontsLoaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded, error]);
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
+    if (!fontsLoaded && !error) {
+        return null;
+    }
 
     return (
-        <Stack>
-            <Stack.Screen
-                name="index"
-                options={{ headerShown: false }}
-            />
-        </Stack>
-    );
-}
+        <GlobalProvider>
 
+            <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+
+                <Stack.Screen name="index" options={{ headerShown: false }} />
+                <Stack.Screen name="search/[query]" options={{ headerShown: false }} />
+            </Stack>
+        </GlobalProvider>
+    );
+};
+
+export default RootLayout;
--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,52 @@
-import { Link } from "expo-router";
+import { Link, Redirect, router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { ScrollView, Text, View, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
+import { useGlobalContext } from "../context/GlobalProvider";
 const app = () => {
+    const { isLoading, isLoggedin } = useGlobalContext()
+    if (!isLoading && isLoggedin) return <Redirect href="/home" />
     return (
         <>
-            <View className="flex justify-center items-center flex-1 ">
-                <Text className="font-pblack text-4xl ">Drop!</Text>
-                <Link href="/home">go to</Link>
-                <StatusBar style="auto" />
-            </View>
+            <SafeAreaView className="bg-primary h-full">
+                <ScrollView contentContainerStyle={{ height: '100%' }}>
+                    <View className='w-full min-h-screen items-center justify-center px-4'>
+                        <Image
+                            src="https://img.icons8.com/fluency/48/drop-shipping.png" alt="drop-shipping"
+                            className="w-12 h-12 rounded-full"
+                        />
+                        <Text className="text-white text-4xl font-pblack">Drop
+                            <Text className="text-secondary">!</Text>
+                        </Text>
+                        <Image
+                            source={images.cards}
+                            className="max-w-[300px] w-full h-[300px]"
+                            resizeMode="contain"
+                        />
+                        <View className="relative mt-5">
+                            <Text className="text-3xl text-white font-psemibold   text-center">
+                                Discover Endless Possibilities with{' '}
+                                <Text className="text-secondary-200 font-psemibold ">
+                                    Drop
+                                </Text>
+                            </Text>
+                            <Image
+                                source={images.path}
+                                className="absolute -bottom-2 -right-8  w-[136px] h-[15px]"
+                                resizeMode="contain"
+                            />
+                        </View>
+                        <CustomButton
+                            title="Continue with Email"
+                            handlePress={() => router.push("/sign-in")}
+                            containerStyles="w-full mt-7"
+                        />
+                    </View>
+                </ScrollView>
+
+            </SafeAreaView>
         </>
     )
 }
